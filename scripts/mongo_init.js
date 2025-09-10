a2423b0894d6bbbf8e51d631cbda14bcbeab4411
srinivas-surya <58618027+srinivas-surya@users.noreply.github.com>
2025-09-10 23:46:17 +0530

    Update mongo_init.js

--- scripts/mongo_init.js
+++ scripts/mongo_init.js
@@ -2,6 +2,6 @@
 db = db.getSiblingDB('inventorydb');
 db.createCollection('products');
 // Expecting a JSON array in the file
-var data = cat('/docker-entrypoint-initdb.d/inventory_sample.json');
+var data = cat('/samples/inventory_sample.json');
 var docs = JSON.parse(data);
 db.products.insertMany(docs);
